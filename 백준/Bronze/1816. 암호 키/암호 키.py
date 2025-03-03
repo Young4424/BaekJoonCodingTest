7f1708fe56cce42055dda85f120c0755b634990c
Young Hwi Kim <42575458+Young4424@users.noreply.github.com>
2025-03-03 21:51:24 +0900

    [Bronze I] Title: 암호 키, Time: 1404 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1816. 암호 키/암호 키.py
@@ -0,0 +1,13 @@
+# 100만보다 작고 2이상의 약수를 가지고 있다면 틀린 비밀번호:
+
+test = int(input())
+
+for item in range(test):
+    number = int(input()) 
+
+    for i in range(2,1_000_001):
+        if number%i == 0:  # 100만 이하의 약수가 존재한다.
+            print("NO")
+            break
+        if i == 1_000_000: # 100만 이하의 약수가 존재하지 않는다.
+            print("YES")
